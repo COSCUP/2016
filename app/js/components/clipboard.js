ed9b4966ec1d0ec28f731fe2c0bd551da72b9860
Sunset <s2821d3721@gmail.com>
2016-06-29 23:12:33 +0800

    Update# clipboard button text

--- app/js/components/clipboard.js
+++ app/js/components/clipboard.js
@@ -2,6 +2,8 @@
 var React    = require('react');
 var ReactDOM = require('react-dom');
 
+var langStore = require('stores/lang.js');
+
 // Implement index page
 var Clipboard = React.createClass({
 	clickHandler: function() {
@@ -16,7 +18,18 @@ var Clipboard = React.createClass({
 			}
 		}
 	},
+    getInitialState: function() {
+        return {lang: langStore.getState()};
+    },
+    langChangeHandler: function() {
+        this.setState({lang: langStore.getState()});
+    },
+    componentDidMount: function() {
+        langStore.register(this.langChangeHandler);
+    },
     render: function() {
+    	var lang  = this.state.lang;
+    	var btnText = (lang=='en')? 'Copy Link' : '複製連結';
     	var text  = this.props.text || '';
     	var style = {
     		display: (text==='')? 'none' : 'block'
@@ -24,7 +37,9 @@ var Clipboard = React.createClass({
         return (
         	<section role="clipboard" style={style} >
         		<input ref="clipboardInput" value={text} />
-        		<button ref="clipboardHelper" onClick={this.clickHandler}>Copy Link</button>
+        		<button ref="clipboardHelper" onClick={this.clickHandler}>
+        			{btnText}
+        		</button>
         	</section>
         );
     }
